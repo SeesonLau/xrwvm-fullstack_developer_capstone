8902fc891d0aaf0d3656853754a100fe30cfb33e
John Laurence Sison <seesonjohnlau@gmail.com>
2026-03-08 19:44:39 +0800

    Section 5: dynamic React pages complete

--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -10,9 +10,8 @@ const Dealers = () => {
   let [states, setStates] = useState([])
 
   // let root_url = window.location.origin
-  let dealer_url ="/djangoapp/get_dealers";
-  
-  let dealer_url_by_state = "/djangoapp/get_dealers/";
+let dealer_url = "/djangoapp/get_dealerships";
+let dealer_url_by_state = "/djangoapp/get_dealerships/";
  
   const filterDealers = async (state) => {
     dealer_url_by_state = dealer_url_by_state+state;
