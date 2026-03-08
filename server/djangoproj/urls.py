44a72984cd4104468963acdb5e48c456941a560c
John Laurence Sison <seesonjohnlau@gmail.com>
2026-03-08 16:23:50 +0800

    Section 1: static pages and Django setup

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -19,6 +19,9 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
+path('about/', TemplateView.as_view(template_name="About.html")),
+path('contact/', TemplateView.as_view(template_name="Contact.html")),
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
